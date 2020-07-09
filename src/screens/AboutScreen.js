a29c8ee33714ca29dc919f0b2c4a38232736dd90
Tamara_M <malahova-tamara@mail.ru>
2020-07-09 13:30:27 +0200

    Navigation in app was improved

--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -1,18 +1,37 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import { AppHeaderIcon } from '../components/AppHeaderIcon';
 
 export const AboutScreen = ({}) => {
     return (
         <View style={styles.center}>
-            <Text>AboutScreen</Text>
+            <Text>It is the best app for personal sketchs.</Text>
+            <Text>App's version is <Text style={styles.version}>1.0.0</Text> </Text>
         </View>
     )
 }
+
+AboutScreen.navigationOptions = ({ navigation }) => ({
+    headerTitle: 'About app',
+    headerLeft: (
+        <HeaderButtons HeaderButtonComponent={AppHeaderIcon}>
+            <Item
+                title='Toggle Drawer'
+                iconName='ios-menu'
+                onPress={() => navigation.toggleDrawer()} />
+        </HeaderButtons>
+    )
+});
 
 const styles = StyleSheet.create({
     center: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    version: {
+        /* fontFamily: 'open-bold', */
+        fontWeight: 'bold'
     }
 })
--- src/screens/PostScreen.js
+++ src/screens/PostScreen.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import {View, Text, StyleSheet, Image, Button, ScrollView, Alert} from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import { DATA } from '../data';
 import { THEME } from '../theme';
+import { AppHeaderIcon } from '../components/AppHeaderIcon';
 
 export const PostScreen = ({navigation}) => {
     const postId = navigation.getParam('postId');
@@ -36,8 +38,17 @@ export const PostScreen = ({navigation}) => {
 
 PostScreen.navigationOptions = ({navigation}) => {
     const date = navigation.getParam('date');
+    const booked = navigation.getParam('booked');
+    const iconName = booked ? 'ios-star' : 'ios-star-outline';
     return {
-        headerTitle: `Post from ${new Date(date).toLocaleDateString()}`
+        headerTitle: `Post from ${new Date(date).toLocaleDateString()}`,
+        headerRight: (
+            <HeaderButtons HeaderButtonComponent={AppHeaderIcon}>
+                <Item
+                    title='Take photo'
+                    iconName={iconName}
+                    onPress={() => console.log('press photo')} />
+            </HeaderButtons>)
     }
 }
 
